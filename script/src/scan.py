7ceb11d67f1f0b0128e35ffbc38f9bbc2f1dea8f
franciscogproenca <55789442+franciscogproenca@users.noreply.github.com>
2020-10-29 17:50:06 +0000

    Add files via upload

---
+++ script/src/scan.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import LaserScan
+
+def callback(msg):
+    #rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    for x in msg.ranges:
+	#print(x)
+	if x < 0.8 and x != 0:
+	   print (x)
+	   print("I'm very close to an objet or a wall!!!")
+	   #stop robot
+	   break
+
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('scan_values', anonymous=True)
+    rospy.Subscriber('/scan_combined', LaserScan, callback)
+
+   
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
